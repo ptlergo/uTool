d206a5213fe721429c1f03dd33d86612cd2f2e31
BHess2653 <GM_Dahaka@BHMac.local>
2016-06-23 17:06:45 -0400

    commit uTool

---
+++ src/utool.js
@@ -0,0 +1,21 @@
+exports.debug = (title, obj) => {
+  const colors = require('colors');
+  const fs = require('fs');
+  const seperator = '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n';
+  const time = new Date();
+  colors.setTheme({
+    create: 'green',
+    read: 'yellow',
+    update: 'magenta',
+    delete: 'red',
+    error: 'red'
+  });
+  const output = seperator + title + '\n' + time + seperator + JSON.stringify(obj);
+
+  if (process.env.DEBUG) {
+    fs.appendFile('logs/eLog.log', output, 'utf8', (err) =>{
+      if (err) throw err;
+    });
+    console.log(output);
+  }
+};
