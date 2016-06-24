377e71af8a06bc5edc8040de951092e30f4d8f75
ptlergo <ptlergo@gmail.com>
2016-06-24 13:16:16 -0400

    clean up comments

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,11 @@ gulp.task('default', () => {
 
 gulp.task('patch', () => {
 
-  // // // grab package.json file
-  // const pack = gulp.src('./package.json');
-  // const tag = '1.0.1';
-  // const newtag = utool.patch(tag);
-  // // const parsed = JSON.parse(pack);
-  // console.log(pack);
-
   // read package.json file
   const currV = package.version;
   console.log(currV);
+  const newtag = utool.patch(tag);
+
 
 
 });
